8c010513d3a365f7ad3091213fd24b3452aada03
AlejandroHernandez1802 <alejandro.herqui@gmail.com>
2023-03-05 11:28:44 -0500

    Adding pug template manager to the project.

--- server.js
+++ server.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+/*Server creation */
 const server = app.listen(port, () => {
   console.log("Server listening on port 3001!!!")
+})
+
+
+/*Templates configuration*/
+app.set("view engine", "pug");
+app.set("views", "views");
+
+/*Rendering content after server creation*/
+app.get("/", (req, res, next) => {
+  res.status(200);
+
+  var payload = {
+    moduleName:"Home"
+  }
+
+  res.render('Home', payload);
 })
